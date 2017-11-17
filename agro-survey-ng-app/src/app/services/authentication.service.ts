399b2adef9de324f775f6ccc861981f2d5c9ea57
Killer <tejaswini.bubby0104@gmail.com>
2017-11-16 19:22:25 -0800

    Connecting to server.

--- agro-survey-ng-app/src/app/services/authentication.service.ts
+++ agro-survey-ng-app/src/app/services/authentication.service.ts
@@ -3,13 +3,18 @@ import {Http, Headers, Response} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
+const REST_API_URL_BASE = 'http://0.0.0.0:8080';
+
 @Injectable()
 export class AuthenticationService {
   constructor(private http: Http) {}
 
   login(username: string, password: string) {
     return this.http
-      .post('/login', JSON.stringify({username: username, password: password}))
+      .post(
+        REST_API_URL_BASE + '/login',
+        JSON.stringify({username: username, password: password}),
+      )
       .map((response: Response) => {
         // login successful if there's a jwt token in the response
         let user = response.json();
