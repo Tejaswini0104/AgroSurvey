312d0d9da22a64f723c5c6dbcbf18c8fce3e3ae7
Killer <tejaswini.bubby0104@gmail.com>
2017-11-16 18:40:26 -0800

    Login page is showing.

--- agro-survey-ng-app/src/app/app.module.ts
+++ agro-survey-ng-app/src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
+// used to create fake backend
+import {BaseRequestOptions} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {routing} from './app.routing';
 
+import {AuthGuard} from './guards/index';
+import {AuthenticationService} from './services/index';
+import {HomeComponent} from './home/index';
+import {LoginComponent} from './login/index';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  imports: [BrowserModule, FormsModule, HttpModule, routing],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
+  providers: [
+    AuthGuard,
+    AuthenticationService,
+
+    // providers used to create fake backend
+    BaseRequestOptions,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
